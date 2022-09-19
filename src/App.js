4bde400ba528957f2a9917cc3d992c483286b547
KaviArasu_NS <KaviArasu_NS@icloud.com>
2022-09-19 21:39:45 +0530

    heroku url update

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
+  uri: "https://graphql-server-kavi.herokuapp.com/graphql",
   cache,
 });
 
